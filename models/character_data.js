ebc2e16a6883b28a62dab42db76bb9fcba8c49b8
Lari Löfman <larwazor@gmail.com>
2023-08-24 15:34:43 +0300

    add character data schema

---
+++ models/character_data.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const characterDataSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  currency: {
+    money: Number
+  }
+})
+
+characterDataSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+  }
+})
+
+userSchema.plugin(uniqueValidator);
+
+const User = mongoose.model('CharacterData', characterDataSchema)
+
+module.exports = User
