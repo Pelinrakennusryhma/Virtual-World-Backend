a77d2e7fd3053ebbaad2f732fd402740e63f6232
Lari Löfman <larwazor@gmail.com>
2023-09-22 12:01:26 +0300

    rename inventory actios

--- routes/inventory.js
+++ routes/inventory.js
@@ -13,14 +13,14 @@ inventoryRouter.get('/:userId', async (request, response) => {
   }
 })
 
-inventoryRouter.patch('/:userId', async (request, response) => {
+inventoryRouter.put('/:userId', async (request, response) => {
   const { itemId, itemName, operation, amount } = request.body
   const userId = request.params.userId
 
   let item
-  if (operation == "add") {
+  if (operation == "ADD") {
     item = await AddItem(userId, itemId, itemName, amount)
-  } else if (operation == "remove") {
+  } else if (operation == "REMOVE") {
     item = await RemoveItem(userId, itemId, amount)
   }
 
